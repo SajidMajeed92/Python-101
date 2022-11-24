32419f040b7b58a88cc364fe040d1fe6f03f716d
SajidMajeed92 <sajidmajeed.ist@gmail.com>
2022-11-24 16:04:11 +0500

    Condtional statement

--- Conditional_Statements.py
+++ Conditional_Statements.py
@@ -3,3 +3,19 @@ i = 10
 if (i > 15):
 	print("10 is less than 15")
 print("I am Not in if")
+
+# python program to illustrate If statement by getting input from the user
+number = int(input("Enter Your Desired Number which you want to check:"))
+if (number > 15):
+	print("Number is greater than 15")
+print ("I am Not in if")
+
+# python program to illustrate If else statement
+i = 20
+if (i < 15):
+    print ("i is Smaller than 15")
+    print ("i'm in if Block")
+else:
+	print("i is Greater than 15")
+	print("i'm in else Block")
+print("I'm not in if and not in else Block")
