bd4802f4f4c0bcd77efb54f77f152254d5da705a
SajidMajeed92 <sajidmajeed.ist@gmail.com>
2022-12-02 16:02:13 +0500

    Condtional statement

--- Conditional_Statements.py
+++ Conditional_Statements.py
@@ -4,12 +4,6 @@ if (i > 15):
 	print("10 is less than 15")
 print("I am Not in if")
 
-# python program to illustrate If statement by getting input from the user
-#number = int(input("Enter Your Desired Number which you want to check:"))
-#if (number > 15):
-#	print("Number is greater than 15")
-#print ("I am Not in if")
-
 # python program to illustrate If else statement
 i = 20
 if (i < 15):
@@ -20,7 +14,6 @@ else:
 	print("i'm in else Block")
 print("I'm not in if and not in else Block")
 
-# Example 2: Python Nested if
 # python program to illustrate nested If statement
 i = 10
 if (i == 10):
@@ -35,3 +28,58 @@ if (i == 10):
 	else:
 		print("i is greater than 15")
 
+# A school has following rules for Grading System:
+# a. Below 25 - F
+# b. 25 to 45 - E
+# c. 45 to 50 - D
+# d. 50 to 60 - C
+# e. 60 to 80 - B
+# f. Above 80 - A
+# Ask user to enter Marks and print the corresponding Grade.
+
+marks = input("Enter Your Marks:")
+if marks<25:
+  print "F"
+elif marks>=25 and marks<45:
+  print "E"
+elif marks>=45 and marks<50:
+  print "D"
+elif marks>=50 and marks<60:
+  print "C"
+elif marks>=60 and marks<80:
+  print "B"
+else:
+  print "A"
+
+#Take values of length and breadth of a rectangle from user and check if it is square or not.
+
+print "Enter length"
+length = input()
+print "Enter breadth"
+breadth = input()
+if length == breadth:
+  print "Yes, it is square"
+else:
+  print "No, it is only Rectangle"
+
+
+#Take input of age of 3 people by user and determine oldest and youngest among them.
+
+first_person_age = input("Enter first person age:")
+second_person_age  = input("Enter Second person age:")
+third_person_age  = input("Enter third person age:")
+
+if first_person_age >= second_person_age and first_person_age >= third_person_age:
+  print "Oldest is",first_person_age
+elif second_person_age >= first_person_age and second_person_age >= third_person_age:
+  print "Oldest is",second_person_age
+elif third_person_age >= first_person_age and third_person_age >= second_person_age:
+  print "Oldest is",third_person_age
+else:
+  print "All are equal"
+#Check you are Eligible for vote
+age = input("Enter Your age:")
+if age>=18:
+    print("Eligible for Vote")
+else:
+    print("Not Eligible for Vote")
